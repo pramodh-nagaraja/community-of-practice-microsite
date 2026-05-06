1675bc844a0593f7c00c45a01ad5a1feca5c6866
Nagaraja <pramodh.nagaraja@accenture.com>
2026-05-06 17:30:07 +0530

    Publish built site to GitHub Pages docs folder

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,9 @@ import { viteSingleFile } from 'vite-plugin-singlefile'
 
 export default defineConfig({
   plugins: [react(), viteSingleFile()],
-  base: './',
+  base: '/community-of-practice-microsite/',
   build: {
+    outDir: 'docs',
     assetsInlineLimit: 10_000_000,
     cssCodeSplit: false,
   },
